c65860b060cd12921d943b4732991906b7fa09bf
book <book@macbook-pro.dlink>
2016-05-19 21:24:50 +0500

    Initial Commit

---
+++ OpenWeatherMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenWeatherMap
+//
+//  Created by book on 19.05.16.
+//  Copyright © 2016 YuriyCraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OpenWeatherMap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OpenWeatherMap
+//
+//  Created by book on 19.05.16.
+//  Copyright © 2016 YuriyCraft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
